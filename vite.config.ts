deb9028839ef1ffc3338b27ad1329e6bf8f1b7bf
cj4mm <junsoo511@naver.com>
2025-04-10 16:32:35 +0900

    🚀 fix: 기능 추가: iOS 흰 화면 대응 최종 배포

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "./", // ✅ iOS Safari 대응을 위한 상대 경로 설정
+  base: "./",
   plugins: [react()],
   build: {
     outDir: "dist",
+    target: "esnext", // ← 이거 추가
   },
 });
+
