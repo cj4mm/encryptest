c3da89a19881b192a338b3976cb63b760f45d408
cj4mm <junsoo511@naver.com>
2025-04-10 15:52:20 +0900

    🛠️ fix: 기능 추가: 브라우저 주소창 입력 시 출력불가 개선-ver2

--- src/App.tsx
+++ src/App.tsx
@@ -120,13 +120,13 @@ export default function App() {
 
   return (
     <div className="max-w-2xl mx-auto p-4 space-y-4">
-      <head>
+      {/* <head>
         <link rel="manifest" href="/manifest.json" />
         <link rel="icon" href="/icon-512.png" />
         <meta name="theme-color" content="#ffffff" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-      </head>
+      </head> */}
 
       <h1 className="text-xl font-bold text-center text-indigo-600">🧠 모질띨빡 암호기</h1>
 
