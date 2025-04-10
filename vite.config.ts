f464b20d8a40b9ece94427398cafd50e68da17f9
cj4mm <junsoo511@naver.com>
2025-04-10 16:10:05 +0900

    🛠️ fix: 기능 추가: 브라우저 주소창 입력 시 출력불가 개선-ver3

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "./", // ✅ iOS Safari 대응을 위한 상대 경로 설정
   plugins: [react()],
   build: {
     outDir: "dist",
